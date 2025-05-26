11f36b1afeb1784ed93d8ba356450cb77212679f
cupcak3z <echaosenpai@gmail.com>
2025-05-26 09:12:22 +0800

    added requirements

--- api.py
+++ api.py
@@ -12,14 +12,15 @@ def get_app_description():
 	return (
     	"Welcome to the TokoSawit API!"
     	"This API allows you to get responses from the RecSys Engine and the Chatbot."
-    	"Use the '/predict/' endpoint with a POST request to make predictions."
-    	"Example usage: POST to '/predict/' with JSON data containing sepal_length, sepal_width, petal_length, and petal_width."
 	)
 
 # Create the API endpoint to get the app description
 @app.get("/")
 async def root():
 	return {"message": get_app_description()}
+
+
+
 
 # Create API endpoint to get user recommendations (make changes to this pls)
 @app.get("/api/v1/ecommerce/recommendations/user/{user_id}")
@@ -30,6 +31,9 @@ async def get_product_recommendations(user_id: str):
     # This includes the product recommendation details, e.g. name, price, etc
     return {"message": f"Product recommendations for user {user_id}"}
 
+
+
+
 # Create the API endpoint to get product recommendations (make changes to this pls)
 @app.get(" /api/v1/ecommerce/recommendations/products/{product_id}")
 async def get_product_recommendations(product_id: str):
@@ -39,6 +43,9 @@ async def get_product_recommendations(product_id: str):
     # This includes the product recommendation details, e.g. name, price, etc
     return {"message": f"Product recommendations for product {product_id}"}
 
+
+
+
 # Create the API endpoint to post the user feedback
 @app.post("/api/v1/ecommerce/recommendations/feedback")
 async def post_feedback():
@@ -46,6 +53,8 @@ async def post_feedback():
 
     # Process is to increment it into the postgre SQL database
     return {"message": f"Feedback received"}
+
+
 
 
 # Create the API endpoint to post the chatbot message
