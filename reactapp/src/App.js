94845a6e1aff6bb295ed9b464c523e0e9fc9f516
tksy1227 <s10256056@connect.np.edu.sg>
2025-06-07 12:42:59 +0800

    commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { ProductProvider } from './contexts/ProductContext';
 import { LanguageProvider } from './contexts/LanguageContext';
